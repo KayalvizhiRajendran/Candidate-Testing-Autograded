86541e6cd9b630177a30fa4fecf3d406a0abacbe
Kayalvizhi Rajendran <kayalvizhi.btech@gmail.com>
2024-05-14 15:51:58 -0500

    added ===

--- candidate-testing.js
+++ candidate-testing.js
@@ -20,7 +20,7 @@ let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("What is your name?");
+  candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
@@ -35,10 +35,10 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  /*if(candidateAnswer == correctAnswer){
+  /*if(candidateAnswer === correctAnswer){
     console.log("Correct Answer!!");
   }else {
-    console.log("Oops!! Wrong answer! The correct answer is Sally Ride")
+    console.log("Oops!! Wrong answer! The correct answer is Sally Ride");
   }*/
   let noOfCorrectAnswers = 0;
   
